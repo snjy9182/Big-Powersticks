5743c47eed780b8869b734031cf8f65fd21a6b0a
Daniel Sohn <dsohn45@gmail.com>
2017-09-09 20:04:36 -0400

    Cleaned up files

--- _1_Sensor_Acc/_1_Sensor_Acc.ino
+++ _1_Sensor_Acc/_1_Sensor_Acc.ino
@@ -4,39 +4,48 @@
 #include "MMA7660.h"
 MMA7660 accelemeter;
 
-
 void setup()
 {
-	accelemeter1.init();
-  //accelemeter2.init();  
-  //accelemeter3.init();  
-	Serial.begin(9600);
-  int n = 0;
+  accelemeter1.init();
+
+  Serial.begin(9600);
+
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+//  pinMode(2, OUTPUT);
+
+  digitalWrite(0, LOW);
+  digitalWrite(1, HIGH);
+//  digitalWrite(2, HIGH);
+    
+    int n = 0;
 
 }
+
 void loop()
 {
-	int8_t x;
-	int8_t y;
-	int8_t z;
-	float ax,ay,az;
+  int8_t x;
+  int8_t y;
+  int8_t z;
+  float ax,ay,az;
   
-	accelemeter.getXYZ(&x,&y,&z);
-	accelemeter.getAcceleration(&ax,&ay,&az);
+  accelemeter.getXYZ(&x,&y,&z);
+  accelemeter.getAcceleration(&ax,&ay,&az);
 
-  Serial.println("Sensor " + (n + 1) + ":");
-	Serial.println(ax);
-	Serial.println(ay);
-	Serial.println(az);
-	
-	Serial.println("*************");
-
-  n = n + 1;
+    Serial.println("Sensor " + (n + 1) + ":");
+  Serial.println(ax);
+  Serial.println(ay);
+  Serial.println(az);
+  
+  Serial.println("*************");
 
 
 
+  digitalWrite(n, HIGH);
+  digitalWrite((n + 1) % 2, LOW);
 
-	delay(5000);
+  n = (n + 1) % 2;
+
+  delay(200);
 }
-
 
